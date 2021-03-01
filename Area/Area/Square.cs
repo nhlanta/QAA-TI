09554d7248295aee0b9b7b686d9cc2cc75b81c13
Nataliia Khlanta <nataliia.khlanta@ngahr.com>
2021-03-01 19:32:19 +0200

    Area of circle and square calculation logic

---
+++ Area/Area/Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Area
+{
+	public class Square
+	{
+		private double _side;
+		public Square(double side)
+		{
+			_side = side;
+		}
+
+		public double GetArea()
+		{
+			double area = _side * _side;
+			return area;
+		}
+
+	}
+}
