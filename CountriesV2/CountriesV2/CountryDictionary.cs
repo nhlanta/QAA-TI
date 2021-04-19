8479a375cd4d01250a18a0a17cc647f41655c9c6
Nataliia Khlanta <nataliia.khlanta@ngahr.com>
2021-04-19 20:15:23 +0300

    Countries dictionary logic

---
+++ CountriesV2/CountriesV2/CountryDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountriesV2
+{
+	public class CountryDictionary
+	{
+		public Dictionary<int, Country> Countries { get; private set; } = new Dictionary<int, Country>();
+
+		public void PrintCountries()
+		{
+			foreach (KeyValuePair<int, Country> keyValue in Countries)
+			{
+				Console.WriteLine(keyValue.Key + "  " + keyValue.Value.Name + " " + keyValue.Value.IsTelenorSupported);
+			}
+		}
+
+		public void ChangeTelenorOption(string name,bool isTelenorSupported) 
+		{
+			foreach (KeyValuePair<int, Country> keyValue in Countries)
+			{
+				if (keyValue.Value.Name == name)
+				{
+					keyValue.Value.IsTelenorSupported = isTelenorSupported;
+				}
+			}
+		}
+
+		public void PrintTelenorFalseCountries()
+		{
+			foreach (KeyValuePair<int, Country> keyValue in Countries)
+			{
+				if (keyValue.Value.IsTelenorSupported == false)
+				{
+					Console.WriteLine(keyValue.Key + " - " + keyValue.Value.Name + " " + keyValue.Value.IsTelenorSupported);
+				}
+			}
+		}
+
+	}
+}
---
+++ CountriesV2/CountriesV2/MyCsvWriter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace CountriesV2
+{
+	public class MyCsvWriter
+	{
+
+		public void WriteData(string filePath, CountryDictionary countries)
+		{
+			
+			using (StreamWriter writer = new StreamWriter(filePath, false))
+			{
+				foreach (KeyValuePair<int, Country> keyValue in countries.Countries)
+				{
+					writer.WriteLine("{0},{1},{2}", keyValue.Key, keyValue.Value.Name, keyValue.Value.IsTelenorSupported);
+				}
+			}
+		}
+
+	
+	}
+
+}
+
