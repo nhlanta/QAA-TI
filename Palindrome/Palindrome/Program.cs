b1d998b878eb1ace43effd602744e83365d04817
Nataliia Khlanta <nataliia.khlanta@ngahr.com>
2021-04-19 22:30:38 +0300

    Updated due to Oksana's comments

--- Palindrome/Palindrome/Program.cs
+++ Palindrome/Palindrome/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace PalindromV
 {
@@ -6,28 +7,45 @@ namespace PalindromV
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Enter a single word");
-			string input = Console.ReadLine();
-			string userInput = input.ToLower();
+			int attempt = 0;
+			string input;
 
-			while (string.IsNullOrEmpty(input))
+			do
 			{
 				Console.WriteLine("Please enter some word");
-				input = Console.ReadLine();
+				input = Console.ReadLine().ToLower();
+
+				attempt++;
+			}
+			while (attempt < 3 && (string.IsNullOrEmpty(input) || input.Any(char.IsDigit)));
+
+			if (string.IsNullOrEmpty(input) || input.Any(char.IsDigit))
+			{
+				Console.WriteLine("Worse things happen..");
+				Console.WriteLine("Don't worry.");
+				Console.WriteLine("Next time will be better :)");
 			}
 
-			Console.WriteLine("This is " + (IsPalindrome(userInput) == true ? "palindrome" : "not palindrome"));
+			else
+			{
+				Console.WriteLine("This is " + (IsPalindrome(input) ? "palindrome" : "not palindrome"));
+			}
+
 			Console.ReadLine();
 		}
+
 		static bool IsPalindrome(string userInput)
 		{
 			char[] charArray = userInput.ToCharArray();
 
 			for (int i = 0; i < charArray.Length / 2; i++)
+			{
 				if (charArray[i] != charArray[charArray.Length - 1 - i])
 				{
 					return false;
 				}
+			}
+
 			return true;
 		}
 	}
