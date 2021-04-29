ea97c7aba38f55f0110626f803d419cbbfebbbf0
Nataliia Khlanta <nataliia.khlanta@ngahr.com>
2021-04-29 23:09:16 +0300

    To do list logic implementation calculated the total sum of hours to finalize all the tasks displayed tasks with user's given priority displayed tasks which can be done for user's given a number of days

---
+++ ToDoList/ToDoList/Complexity.cs
@@ -0,0 +1,34 @@
+﻿namespace ToDoList
+{
+	public class Complexity
+	{
+		public enum ComplexityLevel
+		{
+			High,
+			Medium,
+			Low
+		}
+
+		public int TimeToExecute
+		{
+			get;
+			private set;
+		}
+
+		public Complexity(ComplexityLevel complexityLevel)
+		{
+			if (complexityLevel is ComplexityLevel.High)
+			{
+				TimeToExecute = 4;
+			}
+			else if (complexityLevel is ComplexityLevel.Medium)
+			{
+				TimeToExecute = 2;
+			}
+			else
+			{
+				TimeToExecute = 1;
+			}
+		}
+	}
+}
---
+++ ToDoList/ToDoList/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ToDoList
+{
+	class Task
+	{
+		private string description;
+		public Complexity Complexity { get; set; }
+		public Priority Priority { get; set; }
+
+		public string Description
+		{
+			get { return description; }
+			set
+			{
+				if (string.IsNullOrEmpty(value))
+				{
+					Console.WriteLine("Please enter some value. Description cannot be empty.");
+				}
+				else
+				{
+					description = value;
+				}
+			}
+		}
+	}
+}
